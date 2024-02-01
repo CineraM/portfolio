9ba2535689db8752af5acba7f8f45d5d6819c69a
Matias <64340009+CineraM@users.noreply.github.com>
2024-02-01 02:08:23 -0500

    partial progress

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['c', 'i', 'n', 'e', 'r', 'a'] // change this later!
+    const jobArray = ['C', 'o', 'm', 'p', 'u', 't', 'e', 'r', ' ', 'S', 'c', 'i', 'e', 'n', 'c', 'e', ' ', 'G', 'r', 'a', 'd'] 
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        // Cleanup function to clear the timeout if the component unmounts
+        return () => clearTimeout(timeoutId);
+      }, []); // Empty dependency array ensures this effect runs only once after initial render
+    
+
+    return (
+        <>
+            <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br/> 
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoS} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
+                    <br />
+                </h1>
+                
+
+                <h2> front end / back end / ... </h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+
+            </div>
+
+            <Logo></Logo>
+            </div>
+            <Loader type="ball-pulse" />
+        </>
+    )
+}
+
+export default Home
